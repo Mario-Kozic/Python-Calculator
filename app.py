5abf68eba4f783f6620e4a7983c067779fb530e5
Mario Kozic <mkozic89@gmail.com>
2022-06-21 15:40:30 -0400

    working on our operation functions

--- app.py
+++ app.py
@@ -12,12 +12,33 @@ e = Entry(root, width=50, borderwidth=5)
 e.grid(row=0, column=0, columnspan=3, padx=5, pady=5)
 
 def button_click(number):
+    current = e.get()
+    e.delete(0, END)
+    e.insert(0, str(current) + str(number))
 
 def button_clear():
+    e.delete(0, END)
 
 def button_add():
+    first_number = e.get()
+    global f_num
+    global math
+    math = "addition"
+    f_num = int(first_number)
+    e.delete(0, END)
 
 def button_equal():
+    second_number = e.get()
+    e.delete(0, END)
+
+    if math == 'addition':
+        e.insert(0, f_num + int(second_number))
+    if math == 'subtraction':
+        e.insert(0, f_num - int(second_number))
+    if math == 'multiplication':
+        e.insert(0, f_num * int(second_number))
+    if math == 'division':
+        e.insert(0, f_num / int(second_number))
 
 def button_subtract():
 
